421730bacb30c8431a717a99dc341af828ad2fa7
jforster <jforster>
2015-11-24 17:30:50 +0000

    End of day push

--- app/models/Item.scala
+++ app/models/Item.scala
@@ -10,7 +10,10 @@ object Item {
       Item(1, "Gnome", 20, 10.00),
       Item(2, "King Gnome", 1, 1000.00),
       Item(3, "Prince Gnome", 5, 150.00),
-      Item(4, "Princess Gnome", 3, 150.00))
+      Item(4, "Princess Gnome", 3, 150.00),
+      Item(5, "Small Jacuzzi", 1, 500.00),
+      Item(6, "Oak Jacuzzi", 2, 750.00),
+      Item(7, "Lost Elf", 99, 0.99))
       
   def findAll = items.toList.sortBy(_.itemID)
   
@@ -18,7 +21,7 @@ object Item {
   
   def formatPrice(input: Double):String = {
     import java.text.DecimalFormat
-    val df = new DecimalFormat("#.00")
+    val df = new DecimalFormat("#0.00")
     df.format(input)
   }
   
